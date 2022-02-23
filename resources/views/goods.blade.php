44f1604496e6f8559a58c9b4f7e18f7c06db4420
Dmitrij Kunts <r206265@gmail.com>
2022-02-23 10:24:58 +0200

    rel="nofollow"

--- resources/views/goods.blade.php
+++ resources/views/goods.blade.php
@@ -5,7 +5,7 @@
             @foreach ($goods as $good)
                 <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                        <a href="{{ route('good', $good) }}">
+                        <a rel="nofollow" href="{{ route('good', $good) }}">
                             <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{!! $good->thumbnail() !!}"
                                 alt="blog">
                         </a>
@@ -19,7 +19,7 @@
                                 {{ $good->descKey($cat ?? null) }}
                             </p>
                             <div class="flex items-center flex-wrap ">
-                                <a href="{{ route('good', $good) }}"
+                                <a rel="nofollow" href="{{ route('good', $good) }}"
                                     class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Детальнее
                                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor"
                                         stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
