555b927e7f6e6874787471807029a6f27b21141d
Dmitrij Kunts <r206265@gmail.com>
2022-03-13 18:26:45 +0200

    cats text update

---
+++ app/Console/Commands/CatsTextUpdate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Cat;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class CatsTextUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cats:textupdate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cats text update';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $count = 0;
+        foreach (Cat::all() as $cat) {
+            $textFile = storage_path("texts/{$cat->name}.txt");
+            if (File::exists($textFile)) {
+                $cat->text = File::get($textFile);
+                $cat->save();
+                $count++;
+            }
+        }
+        $this->info("$count updated.");
+        return 0;
+    }
+}
