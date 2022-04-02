e6b6b21921c97168c43c977c80f7b16d9d4e73de
Dmitrij Kunts <r206265@gmail.com>
2022-04-02 21:49:59 +0300

    correct

--- domain-migrate.sh
+++ domain-migrate.sh
@@ -1,4 +1,4 @@
 for domain in $(php artisan domain:list | grep Domain: | cut -d" " -f2); do
     echo ==========$domain===========
-    php artisan migrate --domain=$domain
+    php artisan migrate --domain=$domain --force
 done
