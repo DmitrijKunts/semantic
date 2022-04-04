33635a6a35c13b1e9f9758b7333c4caab0ec0a59
Dmitrij Kunts <r206265@gmail.com>
2022-04-04 14:34:49 +0300

    filters

--- app/Models/Snippet.php
+++ app/Models/Snippet.php
@@ -44,6 +44,7 @@ class Snippet extends Model
             '\.\.\.' => '',
             '\(\d{3}\) \d \d{3} \d{3}' => '',
             '\(\d{3}\) \d{3}-\d{2}-\d{2}' => '',
+            '\(\d{3}\)\d{3}-\d{2}-\d{2}' => '',
         ];
         if (config('app.locale') == 'en') {
             $filters['[а-я]'] = '';
