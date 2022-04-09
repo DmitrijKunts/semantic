52db0a0842de9d742870908570a8ec21bc214904
Dmitrij Kunts <r206265@gmail.com>
2022-04-09 17:48:34 +0300

    block noise

---
+++ app/Http/Middleware/BlockNoise.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class BlockNoise
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $inputs = collect($request->all())->diffKeys(['page' => 1, 'cat_page' => 1, '_token' => 1]);
+        if ($inputs->count()) {
+            if (app()->domain() == 'atletikclub.ru') {
+                dd(request()->headers->get('referer'));
+            } else {
+                abort(410);
+            }
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -41,9 +41,3 @@ Route::get('/imgs/{good:sku}/{index}.jpg', [ImageController::class, 'good'])->wh
 Route::get('/imgs/small/{good:sku}/{index}.jpg', [ImageController::class, 'good'])->whereNumber(['good', 'index'])->name('img.small');
 
 Route::get('/{cat:slug}.html', [CatController::class, 'index'])->name('cat');
-
-Route::get('{any?}', function () {
-    if (app()->domain() == 'atletikclub.ru') {
-        dd(request()->headers->get('referer'));
-    }
-})->where('any', '.*');
