2e48dab54df034f6bcc35a58fc6bd71378f38274
Dmitrij Kunts <r206265@gmail.com>
2022-04-26 09:47:47 +0300

    correct

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -17,7 +17,7 @@ class CatController extends Controller
 
     public function index(Cat $cat, $crawl = false)
     {
-        if ($cat->keys->count() == 0) abort(404);
+        if (blank($cat->keys)) abort(404);
         $catChilds = null;
         if ($cat->childs->count() == 0 && ($cat->feeded == null || $crawl)) {
             Good::makeFromJson(Feed::getFeed($cat->name), $cat);
