55a56709db66281dfe14cef115c2b0021da47813
Dmitrij Kunts <r206265@gmail.com>
2022-05-13 09:27:32 +0300

    show stat before crawl

--- domain-crawl.sh
+++ domain-crawl.sh
@@ -1,4 +1,5 @@
 for domain in $(php artisan domain:list | grep Domain: | cut -d" " -f2); do
     echo ==========$domain===========
+    php artisan stat --domain=$domain
     php artisan cats:crawl --domain=$domain
 done
