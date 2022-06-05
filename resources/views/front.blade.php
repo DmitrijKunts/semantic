b04e8f470efd6a391c7ef1bdc088b83743954eab
Dmitrij Kunts <r206265@gmail.com>
2022-06-05 18:36:06 +0300

    все категории первого уровня на главной

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     @foreach ($menu as $cat)
-        @break($loop->index == 5)
+        {{-- @break($loop->index == 5) --}}
         <div class="text-gray-600 body-font">
             <div class="container px-5 py-4 mx-auto">
                 <div class="text-center mb-10">
